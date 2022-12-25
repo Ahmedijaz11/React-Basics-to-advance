6cc17919f5c7646d5419e958c00f42998d5d28bf
Ahmedijaz11 <101427579+Ahmedijaz11@users.noreply.github.com>
2022-12-26 01:51:29 +0500

    basics part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
+let name = "Ahmed";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Ahmed
-        </a>
-      </header>
-    </div>
+   <>
+
+   <div className="navbar">
+
+   <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact Us</li>
+   </nav>
+
+   </div>
+
+   <div className="Home">
+
+    <h1>Hello I am {name}</h1>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostr
+    um exercitationem ullam corporis suscipit laboriosam, nisi ut
+    aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore
+    eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt
+    mollit anim id est laborum.
+    </p>
+   </div>
+
+
+   <div className="About">
+
+  <h1>About Me</h1>
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut
+  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostr
+  um exercitationem ullam corporis suscipit laboriosam, nisi ut
+  aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore
+  eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt
+  mollit anim id est laborum.
+  </p>
+  </div>
+
+   </>
+    
+
+
   );
 }
 
