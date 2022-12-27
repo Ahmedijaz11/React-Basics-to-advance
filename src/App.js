4c24dcfbc64f3afe6407c4d5d92d112e152c3df6
Ahmedijaz11 <101427579+Ahmedijaz11@users.noreply.github.com>
2022-12-27 10:29:23 +0500

    Basic { navbar created }

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
 let name = "Ahmed";
 function App() {
   return (
    <>
 
-   <div className="navbar">
+      <Navbar bg="black" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-   <nav>
-    <li>Home</li>
-    <li>About</li>
-    <li>Contact Us</li>
-   </nav>
-
-   </div>
+   
 
    <div className="Home">
 
